fef45767352236e0a2c09b7a55330d0af90e37b0
Ajay <ajay.3mindsdigital@gmail.com>
2023-01-26 00:04:39 +0530

    Added middle sections on Home page

--- pages/home.php
+++ pages/home.php
@@ -2,13 +2,13 @@
         <section class="slider-section">
             <div
                 class="carousel"
-                data-flickity='{ "wrapAround": true, "pageDots": false}'>
+                data-flickity='{ "wrapAround": true, "pageDots": false, "autoPlay": true}'>
                 <div class="carousel-cell">
                     <div class="carousel-cell__data">
                         <span class="carousel-cell__text">sweat never looked so sexy</span>
                         <img
                             src="../assets/images/slider-img-1.jpg"
-                            alt=""
+                            alt="Slider Image"
                             class="slider-image" />
                     </div>
                 </div>
@@ -17,7 +17,7 @@
                         <span class="carousel-cell__text">sweat never looked so sexy</span>
                         <img
                             src="../assets/images/slider-img-2.jpg"
-                            alt=""
+                            alt="Slider Image"
                             class="slider-image" />
                     </div>
                 </div>
@@ -26,7 +26,7 @@
                         <span class="carousel-cell__text">sweat never looked so sexy</span>
                         <img
                             src="../assets/images/slider-img-3.jpg"
-                            alt=""
+                            alt="Slider Image"
                             class="slider-image" />
                     </div>
                 </div>
@@ -35,7 +35,7 @@
                         <span class="carousel-cell__text">sweat never looked so sexy</span>
                         <img
                             src="../assets/images/slider-img-4.jpg"
-                            alt=""
+                            alt="Slider Image"
                             class="slider-image" />
                     </div>
                 </div>
@@ -44,10 +44,52 @@
                         <span class="carousel-cell__text">sweat never looked so sexy</span>
                         <img
                             src="../assets/images/slider-img-5.jpg"
-                            alt=""
+                            alt="Slider Image"
                             class="slider-image" />
                     </div>
                 </div>
             </div>
         </section>
         <!-- Slider Section -->
+
+        <!-- Classic Section -->
+        <section class="classic-section">
+            <div class="classic-section__wrapper">
+                <a href="#" class="primary-link">
+                    <span class="primary-link__text">introducing</span> 
+                    the classic collection
+                </a>
+                <div class="classic-section__category">
+                    <a href="#" class="classic-section__link leggings-category">
+                        <span class="classic-section__text">classic</span>
+                        leggings
+                    </a>
+                    <a href="#" class="classic-section__link top-category">
+                        <span class="classic-section__text">classic</span>
+                        sports bra
+                    </a>
+                </div>
+                <a href="#" class="secondary-link">
+                    shop kirgo activewear
+                </a>
+            </div>
+            <p class="classic-section__tagline">made to move with love and sweat in India</p>
+        </section>
+        <!-- Classic Section -->
+
+        <!-- Quote Section -->
+        <section class="quote-section">
+            <div class="quote-section__wrappper">
+                <div class="quote-section__single quote-section__img1">
+                    <!-- <img src="../assets/images/slider-img-2.jpg" alt="Quote Image" class="quote-section__img1"> -->
+                    <img src="../assets/images/text-icon.svg" alt="Stronger Everyday" class="quote-section__textIcon">
+                </div>
+                <div class="quote-section__single quote-section__img2">
+                    <!-- <img src="../assets/images/slider-img-3.jpg" alt="Quote Image" class="quote-section__img2"> -->
+                    <img src="../assets/images/text-icon.svg" alt="Stronger Everyday" class="quote-section__textIcon">
+                </div>
+            </div>
+        </section>
+        <!-- Quote Section -->
+
+
